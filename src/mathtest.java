36854adb052188a11418fd7b4ac3d813f869af84
sookraj.j <sookraj.j@stu.janesville.k12.wi.us>
2023-09-06 11:20:03 -0500

    hi

---
+++ src/mathtest.java
@@ -0,0 +1,27 @@
+public class mathtest {
+
+    public static void main(String[] args) {
+        int twocubed = (int)Math.pow(2, 3); // 2*3
+
+        int num1 = 5;
+        int num2 = 5;
+        int num3 = 5;
+        int num4 = 5;
+        int sum = num1 + num2 + num3 + num4;
+        sum /= 4; // sum = sum / 4
+
+        num1 = num1 + 1;
+        num1 += 1;
+        num1++;
+
+        int test = 3/4;
+        System.out.println(test);
+
+        double myWallet = 235.963475;
+        double myWRounded = Math.round(myWallet * 100.0) / 100.0;
+
+        System.out.println("myWRounded");
+        System.out.printf("myWRounded = %.2\n", myWallet);
+
+    }
+}
