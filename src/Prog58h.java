92590e30badf2be53dff42c237494dd8b44e9488
sookraj.j <sookraj.j@stu.janesville.k12.wi.us>
2023-09-11 11:24:55 -0500

    hi

--- src/Prog58h.java
+++ src/Prog58h.java
@@ -11,13 +11,20 @@ public class Prog58h {
         System.out.println("The interest rate is");
         double r = input.nextDouble();
 
-        System.out.println("ttt");
+        System.out.println("The amount of years money is in the account: ");
         double n = input.nextDouble();
 
         System.out.println("The amount of days the money is in the account is");
         double t = input.nextDouble();
 
-        double A = p * ((1 +((.01 * r)/ n) * (n * t)/ 365));
+        double f = Math.pow(((.01 * r) / n), ((n * t) / 365));
+        double A = p * ((1 + f));
+
+        double earn = A - p;
+
+
+        System.out.println("The interest earned is " + earn);
+        System.out.println("The total amount in savings is now " + A);
 
 
 
---
+++ src/Prog88A.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Prog88A {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        //Math.random() * (max - min) + min
+        int num1 = (int)(Math.random() * (13 - 1)) + 1;
+        int num2 = (int)(Math.random() * (20 - 2)) + 2;
+
+        int sum = num1 + num2;
+        int dif = num1 - num2;
+        int abs = Math.abs(dif);
+        double ave = (num1 + num2) / 2;
+        double pro = num1 * num2;
+        int max, min;
+
+        if (num1 > num2) {
+             max = num1;
+        } else {
+            max = num2;
+        }
+
+
+        if (max == num1) {
+            min = num2;
+        } else {
+            min = num1;
+        }
+        System.out.println("Original numbers are " + num1 + " and " + num2);
+
+        System.out.println("Sum = " + sum + " Difference = " + dif + " product = " + pro);
+        System.out.println("Average = " + ave + " Absolute value " + abs);
+        System.out.println("Maximum = " + max + " Minimum " + min);
+    }
+}
+
+//Original numbers are 2 and 4
+//Sum = 6 Difference = -2 product = 8.0
+//Average = 3.0 Absolute value 2
+//Maximum = 4 Minimum 2
