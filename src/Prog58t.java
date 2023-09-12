0b66b8a9dc4ea511727af4f037d1bd188329fb34
sookraj.j <sookraj.j@stu.janesville.k12.wi.us>
2023-09-12 11:24:16 -0500

    hi

--- src/Prog58t.java
+++ src/Prog58t.java
@@ -1,2 +1,19 @@
+import java.util.*;
+
 public class Prog58t {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("The amount due is: ");
+        double d = input.nextDouble();
+
+        System.out.println("How much money you pay: ");
+        double m = input.nextDouble();
+
+        double t = m - d;
+        System.out.println("The amount of change: ");
+
+
+
+    }
 }
