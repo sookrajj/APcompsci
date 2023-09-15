4f344551eb492d77f4bd99cb6345990c63c97b66
sookraj.j <sookraj.j@stu.janesville.k12.wi.us>
2023-09-15 11:27:32 -0500

    hi

--- src/Prog93a.java
+++ src/Prog93a.java
@@ -6,25 +6,30 @@ public class Prog93a {
         System.out.println("The amount of kilos you used was: ");
         double k = input.nextDouble();
         double f = k * .0475;
-        double t = f * .1;
-        double w = f * .03;
-        double h = f * .04;
-        double p = f + t + w;
-        double s = p + h;
+        double i = Math.round(f * 100) / 100.0;
+        double t = i * .1;
+        double v = Math.round(t * 100) / 100.0;
+        double w = i * .03;
+        double b = Math.round(w * 100) / 100.0;
+        double h = i * .04;
+        double m = Math.round(h * 100) / 100.0;
+        double p = i + v + b;
+        double s = p + m;
 
-        System.out.println("   C O M P S C I Electric   " +
-                "-----------------------------" +
-                "Kilowatts Used             " + k +
-                "-----------------------------" +
-                "" +
-                "Base Rate 993*.475 $ " + f +
-                "Surcharge          $  " + t +
-                "Citytax            $  " + w +
-                "" +
-                "                       ------" +
-                "" +
-                "Pay this amount    $ " + p +
-                "" +
-                "After May 20th Pay $ " + s);
+        System.out.println("   C O M P S C I Electric   ");
+        System.out.println("-----------------------------");
+        System.out.println("Kilowatts Used             " + k);
+        System.out.println("-----------------------------");
+        System.out.println("");
+        System.out.println("Base Rate 993*.475     $ " + i);
+        System.out.println("Surcharge             $  " + v);
+        System.out.println("Citytax               $  " + b);
+        System.out.println("");
+        System.out.println("                       ------");
+        System.out.println("");
+        System.out.println("Pay this amount        $ " + p);
+        System.out.println("");
+        System.out.println("After May 20th Pay     $ " + s);
+
     }
 }
