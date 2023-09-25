98a4047ebcc5d90c137b677231c17ddb1d04a5b4
sookraj.j <sookraj.j@stu.jabesville.k12.wi.us>
2023-09-25 11:25:03 -0500

    hi

---
+++ src/LP1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class LP1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("B  I  N  G  O");
+
+        double b = Math.random() * (16 - 1);
+        double h = Math.floor(b);
+        double i = Math.random() * (31 - 16);
+        double w = Math.floor(i);
+        double n = Math.random() * (46 - 31);
+        double k = Math.floor(n);
+        double g = Math.random() * (61 - 46);
+        double p = Math.floor(g);
+        double o = Math.random() * (76 - 61);
+        double q = Math.floor(o);
+    }
+}
