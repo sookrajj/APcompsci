e39b6a5a0010ce43db80b2185f828c9306d17f24
sookrajj <144150804+sookrajj@users.noreply.github.com>
2023-10-02 20:53:51 -0500

    hi

---
+++ src/LP5_17.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class LP5_17 {
+    public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter password: ");
+      String h = input.nextLine().toLowerCase();
+      if (h == b-day_boy) {
+        System.out.println("Welcome");
+      } else {
+        System.out.println("Try again");
+      }
+       System.out.print("Enter password: ");
+      String h = input.nextLine().toLowerCase();
+      if (h == b-day_boy) {
+        System.out.println("Welcome");
+      } else {
+        System.out.println("Try again");
+      }
+       System.out.print("Last try to enter password: ");
+      String h = input.nextLine().toLowerCase();
+      if (h == b-day_boy) {
+        System.out.println("Welcome");
+      } else {
+        System.out.println("Access denied!");
+      }
+          
+
+    }
+}
