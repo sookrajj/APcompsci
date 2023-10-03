1ff0f5597c3fefc901059b61ed9111fdfcc79ab8
sookrajj <144150804+sookrajj@users.noreply.github.com>
2023-10-02 20:24:34 -0500

    hi

--- src/LP5_5.java
+++ src/LP5_5.java
@@ -3,9 +3,14 @@ import java.util.*;
 public class LP5_5 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a positive integer: ");
+        System.out.println("Enter a positive integer over 100 below 1000: ");
         int y = input.nextInt();
-
+        double r = Math.round(y / 100);
+        double g = Math.round((y - (r * 100)) / 10);
+        double h = Math.round((y - (r * 100)) - (g * 10));
+        System.out.println(r);
+        System.out.println(g);
+        System.out.println(h);
 
     }
 }
