03d13dc71a8cdc9dde18d20348a7c000dbbbe8ed
sookrajj <144150804+sookrajj@users.noreply.github.com>
2023-10-09 13:20:41 -0500

    hi

--- src/Lang54bCl/cl54a.java
+++ src/Lang54bCl/cl54a.java
@@ -8,13 +8,18 @@ public class cl54a {
 
 
 
-
-
-
-
     public Lang54bCl(int num1, int num2, int num3, int num4) {
         mynum1 = num1;
         mynum2 = num2;
         mynum3 = num3;
+        mynum4 = num4;
     }
+
+    public void calc() {
+        sum = mynum1 + mynum2 + mynum3 + mynum4;
+        mean = (mynum1 + mynum2 + mynum3 + mynum4) / 4;
+    }
+
+    public int getSum() {return sum;}
+    public int getMean() {return mean;}
 }
