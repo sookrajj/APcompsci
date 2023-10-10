e6be86b30ddf1e5a6de835d0e43787794e768104
sookraj.j <sookraj.j@stu.jabesville.k12.wi.us>
2023-10-10 11:25:33 -0500

    hi

--- src/Prog285bcl/Prog285b.java
+++ src/Prog285bcl/Prog285b.java
@@ -1,7 +1,26 @@
 package Prog285bcl;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
 
 public class Prog285b {
     public static void main(String[] args) {
+        try {
+            Scanner input = new Scanner(new File("Langdat/prog285b.dat"));
+
+            System.out.println("Id\tCode\tSales\tCommission");
+            while (input.hasNext()) {
+                int id = input.nextInt();
+                int c = input.nextInt();
+                double s = input.nextDouble();
+
+                Salesperson fred = new Salesperson(id, c, s);
+                fred.setcom();
+                System.out.println(fred.toString()); // println(fred)
+            }
+        } catch (IOException e) {
+            System.out.println("Can't find data file.");
+        }
 
     }
 }
