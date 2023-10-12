23918ba3d5e25624ff7e0d194bfdc7910551959e
sookrajj <144150804+sookrajj@users.noreply.github.com>
2023-10-12 14:17:28 -0500

    hi

--- src/Prog76acl/Prog76a.java
+++ src/Prog76acl/Prog76a.java
@@ -1,4 +1,16 @@
 package Prog76acl;
+import java.util.*;
 
 public class Prog76a {
+  Scanner input = new Scanner(System.in);
+
+  System.out.println("A whole number between 1-9");
+  int num = input.nextInt();
+
+  cl76a w = new cl76a(num);
+  w.calc();
+
+  cool = w.getNumber();
+
+  System.out.println("cool number: " + cool);
 }
