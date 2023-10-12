9bc636b56a46dacc595e5a93b063ffc302669b7d
sookrajj <144150804+sookrajj@users.noreply.github.com>
2023-10-12 12:15:29 -0500

    Update Prog54c.java

--- src/Prog54ccl/Prog54c.java
+++ src/Prog54ccl/Prog54c.java
@@ -1,4 +1,18 @@
 package Prog54ccl;
+import java.util.*;
 
 public class Prog54c {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Enter radius: ");
+    double radius = input.nextDouble();
+
+    cl54c wow = new cl54c(radius);
+    wow = wow.calc();
+
+    radius = wow.getRadius();
+    area = wow.getArea();
+    circum = wow.getCircumference();
+    System.out.println("Radius: " + radius + "\nArea: " + area + "\nCircumference: " + circum;
 }
