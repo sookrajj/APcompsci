d2c6b3e78cf73954e5cb7ea859c9940ae55d10f2
sookrajj <144150804+sookrajj@users.noreply.github.com>
2023-10-12 14:12:52 -0500

    hi

--- src/Prog76acl/cl76a.java
+++ src/Prog76acl/cl76a.java
@@ -1,4 +1,20 @@
 package Prog76acl;
 
 public class cl76a {
+  private int y;
+  private int r;
+  private int u;
+
+  public cl76a(int num) {
+    y = num;
+    r = 12345679;
+    u = 9;
+  }
+
+  public void calc() {
+    int cool = y * u;
+    int number = cool * r;
+  }
+
+  public int getNumber() {return number;}
 }
