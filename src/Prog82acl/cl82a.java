68227a4da5e5c30b4633da42a665c6082d93e9ac
sookrajj <144150804+sookrajj@users.noreply.github.com>
2023-10-12 14:24:36 -0500

    hi

--- src/Prog82acl/cl82a.java
+++ src/Prog82acl/cl82a.java
@@ -1,4 +1,21 @@
 package Prog82acl;
 
 public class cl82a {
+  private int y;
+  private int r;
+  private int u;
+  private int t;
+
+  public cl82a(int speed, int limit) {
+    y = speed;
+    r = limit;
+    u = 20;
+    t = 5;
+  }
+
+  public void calc() {
+    fine = u + (r - y) * t;
+  }
+
+  public int getFine() {return fine;}
 }
