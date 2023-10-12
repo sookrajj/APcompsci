be26e113308e90b0a946075d9c23469c36f04008
sookrajj <144150804+sookrajj@users.noreply.github.com>
2023-10-12 14:31:09 -0500

    hi

--- src/Prog82acl/Prog82a.java
+++ src/Prog82acl/Prog82a.java
@@ -1,4 +1,18 @@
 package Prog82acl;
+import java.util.*;
 
 public class Prog82a {
+  Scanner input = new Scanner(System.in);
+  System.out.println("Speed limit: ");
+  int limit = input.nextInt();
+  System.out.println("Your speed: ");
+  int speed = input.nextInt();
+
+  cl82a w = new cl82a(speed, limit);
+  w.calc();
+
+  int fine = getFine();
+
+  System.out.println("Fine-------------" + fine);
+
 }
