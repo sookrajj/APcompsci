48dfd57dd89f8c81340f05bffd65168f014f3d32
sookrajj <144150804+sookrajj@users.noreply.github.com>
2023-10-12 12:09:24 -0500

    hi

--- src/Prog54ccl/cl54c.java
+++ src/Prog54ccl/cl54c.java
@@ -1,4 +1,20 @@
 package Prog54ccl;
 
 public class cl54c {
+  private int rad;
+  private double pie;
+
+  public cl54c(int radius) {
+    rad = radius;
+    pie = 3.14159;
+  }
+
+  public void calc() {
+    double cir = pie * rad * 2;
+    double area = pie * Math.pow(rad, 2);
+  }
+
+  public double getArea() {return area;}
+  public double getCircumference() {return cir;}
+  public double getRadius() {return rad;}
 }
