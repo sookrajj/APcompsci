52b7f2e7723792cc60468609d72b0b3f5795f072
sookrajj <144150804+sookrajj@users.noreply.github.com>
2023-10-17 13:41:13 -0500

    hi

--- src/Prog209a.java
+++ src/Prog209a.java
@@ -3,13 +3,22 @@ import java.io.IOException;
 import java.util.*;
 public class Prog209a {
     public static void main(String[] args) {
-        Scanner input = new Scanner(new File("Langdat/prog215a.dat"));
-
         try {
+            Scanner input = new Scanner(new File("Langdat/prog215a.dat"));
 
-        }catch{
-            System.out.println("Couldn't find data file");
+            while (input.hasNext()) {
+                int u = input.nextInt();
+                int y = 0;
+                int k = 0;
+                int h = 0;
+                if (u >= 500) k++;
+                else h++;
+                if (u >= 0) y++;
+                else y++;
+                System.out.println("The number of numbers less than 500 is " + h + "\nThe number of numbers greater than or equal to 500 is " + k + "\nThe total number of numbers is " + y);
 
-        }
+            }
+        } catch (IOException e) {
+            System.out.println("Can't find data file.");
     }
 }
