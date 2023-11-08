6d025f5b572d76008e1f4f1edf76755cb3588f0b
sookrajj <144150804+sookrajj@users.noreply.github.com>
2023-11-08 17:37:11 -0600

    hi

--- src/Q2/prog410tacl/prog410t.java
+++ src/Q2/prog410tacl/prog410t.java
@@ -9,11 +9,36 @@ public class prog410t {
     try {
       Scanner s = new Scanner(new File("Langdat/survey2.dat"));
       Scanner s2 = new Scanner(new File("Langdat/survey.dat"));
-
+      int cnt = 1;
+      int[] ret = new int[100];
+      int[] ho = new int[50];
+      double mean = 0.0;
+      double pov = 0.0;
 
       while (s.hasNext()) {
-
+        int id = s.nextInt();
+        int incs = s.nextInt();
+        int mems = s.nextInt();
+        for (int lcv = 0; lcv < cnt; lcv++) {
+          cl410tacl cool = new cl410tacl(ids, incs, mems);
+          ret[lcv] = cool;
+        }
+        cnt++;
       }
+      mean = cool.getTot();
+      ho = cool.getho();
+      pov = cool.getpov();
+      
+      System.out.println("id     income    members");
+      for (int lcv = 0; lcv < cnt; lcv++) {
+        System.out.println(ret[lcv]);
+      }
+      System.out.println("Households with income exceeding an average income of " + mean + "\nid     income    members");
+      for (int lcv = 0; lcv < cnt; lcv++) {
+        System.out.println(ho[lcv]);
+      }
+      System.out.println("Percent of households below poverty level = " + pov);
+      
     } catch (IOException e) {
       System.out.println("Can't find data file.");
     }
