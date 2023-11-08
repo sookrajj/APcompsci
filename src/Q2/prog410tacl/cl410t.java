dc84d174ca9027bc92e22c30d01fbb7f7ebcb7f2
sookrajj <144150804+sookrajj@users.noreply.github.com>
2023-11-08 17:37:00 -0600

    hi

--- src/Q2/prog410tacl/cl410t.java
+++ src/Q2/prog410tacl/cl410t.java
@@ -4,11 +4,17 @@ public class cl410t {
   private int[] myids;
   private int[] myincs;
   private int[] mymems;
+  private int[] myho;
   private double tot;
   private int cnt;
+  private int yes;
+  private int no;
   private double bwpv;
+  private double mypov;
 
   public cl410t(int ids, int incs, int mems) {
+    int yes = 0;
+    int no = 0;
     cnt = 0;
     cnt++;
     for (int lcv = 0; lcv < cnt; lcv++) {
@@ -19,13 +25,23 @@ public class cl410t {
     tot = 0.0;
     tot += incs;
     bwpv = 0.0;
+    mypov = 0.0;
   }
 
   public void calc() {
     tot = tot/cnt;
-    
+    for (int lcv = 0; lcv < cnt; lcv++) {
+      if (myincs[lcv] > tot) {
+        myho[lcv] += myincs[lcv];
+      }
+      if (incs[lcv] > 3750.00 + 750.00 * (mymems[lcv]-2)) yes++;
+      else no++;
+    }
+    mypov = no/(yes+no);
   }
+  public double getpov() {return mypov;}
   public double getTot() {return tot;}
+  public int[] getHo() { return myho[];}
   public String toString () {
     for (int lcv = 0; lcv < cnt; lcv++) {
       return myids[lcv] + "\t" + myincs[lcv] + "\t" + mymems[lcv];
