b490aa24b1e9597c9b9bb01bc6e6a83792eeea30
sookrajj <144150804+sookrajj@users.noreply.github.com>
2023-11-08 10:04:12 -0600

    hi

---
+++ src/Q2/prog410t.java
@@ -0,0 +1,16 @@
+package Q2;
+
+import java.util.*;
+import java.io.File;
+import java.io.Exception;
+
+public class prog410t {
+  public static void main(String[] args) {
+    try {
+      Scanner s = new Scanner(new File("Langdat/survey2.dat"));
+      Scanner s2 = new Scanner(new File("Langdat/survey.dat"));
+      
+
+      while (s.hasNext()) {
+        
+    }
