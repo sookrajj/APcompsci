136eaa6435be171db52c1e0f973169c874d4c472
sookrajj <144150804+sookrajj@users.noreply.github.com>
2023-11-07 18:22:53 -0600

    hi

---
+++ src/Q2/cl402a.java
@@ -0,0 +1,13 @@
+package Q2;
+
+public class cl402a {
+  private int myid;
+  private int myscore;
+  
+  public cl435g(int score, int id) {
+        myscore = score;
+        myid = id;
+  }
+
+    public String toString() { return myid + "\t\t" + myscore; }
+}
