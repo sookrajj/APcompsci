35e838d8b22bbe0dab1183f08e68a741e54c1124
sookrajj <144150804+sookrajj@users.noreply.github.com>
2023-11-09 20:54:30 -0600

    hi

---
+++ src/Q2/cllp3-10.java
@@ -0,0 +1,32 @@
+package Q2;
+
+public class cllp3-10 {
+  private int myby;
+  private int mybm;
+  private int mybd;
+  private int mycy;
+  private int mycm;
+  private int mycd;
+  private int alive;
+  private int slpt;
+
+  public cllp3-10(int by, int bm, int bd, int cy, int cm, int cd) {
+    myby = by;
+    mybm = bm;
+    mybd = bd;
+    mycy = cy;
+    mycm = cm;
+    mycd = cd;
+    alive = 0;
+    slpt = 0;
+  }
+
+public void calc() {
+  alive = Math.abs(myby-mycy) * 365 + Math.abs(mybm-mycm) * 30 + Math.abs(mybd-mycd);
+  slpt = (alive/2) * 8;
+}
+
+public String toString() {
+  return "You have been alive for " + alive + "days\nYou have slept " + slpt + "hours";
+}
+}
