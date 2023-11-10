cdc1b312960f0e7651d67c0d3ba78ab68f98ce4f
sookrajj <144150804+sookrajj@users.noreply.github.com>
2023-11-09 21:48:12 -0600

    hi

---
+++ src/Q2/lp3-14.java
@@ -0,0 +1,26 @@
+package Q2;
+import java.util.*;
+
+public class lp3-14 {
+  public static void main(String[] args) {
+    Scanner turkeygogobblegobble = new Scanner(System.in);
+    System.out.println("Enter results for New York: ");
+    System.out.print("Aubrey: ");
+    int any = turkeygogobblegobble.nextInt();
+    System.out.print("Martinez: ");
+    int mny = turkeygogobblegobble.nextInt();
+    System.out.println("Enter results for New Jersey: ");
+    System.out.print("Aubrey: ");
+    int anj = turkeygogobblegobble.nextInt();
+    System.out.print("Martinez: ");
+    int mnj = turkeygogobblegobble.nextInt();
+    System.out.println("Enter results for Connecticut: ");
+    System.out.print("Aubrey: ");
+    int ac = turkeygogobblegobble.nextInt();
+    System.out.print("Martinez: ");
+    int mc = turkeygogobblegobble.nextInt();
+    cllp3-14 turkeydied = new cllp3-14(any, mny, anj, mnj, ac, mc);
+    String turkeysmydinnernow = turkeydied;
+    System.out.print("\n" + turkeysmydinnernow);
+  }
+}
