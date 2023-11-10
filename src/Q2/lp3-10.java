90e053c7610625e528c91bb16b09e22907c64ce5
sookrajj <144150804+sookrajj@users.noreply.github.com>
2023-11-09 20:54:51 -0600

    hi

---
+++ src/Q2/lp3-10.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class lp3-10 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    String hi = "";
+    System.out.println("Enter your birth date");
+    System.out.println("Year ");
+    int by = input.nextInt();
+    System.out.print("\nMonth ");
+    int bm = input.nextInt();
+    System.out.println("\nDays ");
+    int bd = input.nextInt();
+    System.out.print("\nEnter today's date");
+    System.out.print("\nYears ");
+    int cy = input.nextInt();
+    System.out.print("\nMonths ");
+    int cm = input.nextInt();
+    System.out.print("\nDays ");
+    int cd = input.nextInt();
+    cllp3-10 sad = new cllp3-10(by, bm, bd, cy, cm, cd);
+    sad.calc();
+    h = sad;
+    System.out.println("\n" + sad);
+  }
+}
+    
+    
+    
+    
