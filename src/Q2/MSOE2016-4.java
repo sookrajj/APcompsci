8278d5d62b338f2021ef176e9b9c03f142e75dbd
sookrajj <144150804+sookrajj@users.noreply.github.com>
2023-11-09 22:01:17 -0600

    hi

---
+++ src/Q2/MSOE2016-4.java
@@ -0,0 +1,9 @@
+package Q2;
+import java.util.*;
+public class MSOE2016-4 {
+  public static void main(String[] args) {
+
+    double turkeywithsomestuffin = Math.round(Math.random() * 10000);
+
+    while (turkeywithsomestuffin > 1)
+      
