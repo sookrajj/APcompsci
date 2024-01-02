1c696ff223d018abbf315e6b838ae3a21a20542b
sookrajj <144150804+sookrajj@users.noreply.github.com>
2024-01-02 12:34:47 -0600

    hi

--- src/Q2/prog470b.java
+++ src/Q2/prog470b.java
@@ -9,15 +9,24 @@ public class prog470b {
         try {
             Scanner s = new Scanner(new File("Langdat/prog464a.dat"));
             int[][] mat = new int[5][5];
+            int mtot = 0;
+            int otot = 0;
             System.out.println("Original Matrix");
             for (int lcv = 0; lcv < mat.length; lcv++) {
                 for (int lcv2 = 0; lcv2 < mat[0].length; lcv2++) {
                     mat[lcv][lcv2] = s.nextInt();
                     System.out.print(mat[lcv][lcv2] + " ");
+                    if (lcv == lcv2) mtot += mat[lcv][lcv2];
+                    else if (lcv == 0 && lcv2 == 4) stot += mat[lcv][lcv2];
+                    else if (lcv == 1 && lcv2 == 3) stot += mat[lcv][lcv2];
+                    else if (lcv == 2 && lcv2 == 2) stot += mat[lcv][lcv2];
+                    else if (lcv == 3 && lcv2 == 1) stot += mat[lcv][lcv2];
+                    else if (lcv == 4 && lcv2 == 0) stot += mat[lcv][lcv2];
                 }
                 System.out.println();
             }
-
+            
+            System.out.print("\nThe main diagnal total is " + mtot + "\nThe other diagnal total is " + stot);
 
         } catch (
                 IOException e) {
