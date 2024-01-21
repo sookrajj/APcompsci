9e94565db2edd098641f506031d216c86e1cbf0f
sookrajj <144150804+sookrajj@users.noreply.github.com>
2024-01-21 13:55:29 -0600

    hi

---
+++ src/cows.java
@@ -0,0 +1,24 @@
+
+public cows {
+  private int myWeight;
+	private int myPounds;
+	private int myCorn;
+	private int myNumBales;
+
+  public horse(int cowWeight, int poundsOfMilk, int cowcorn, int cowhay) {
+    myWeight = cowWeight;
+    myPounds = poundsOfMilk;
+    myCorn = cowcorn;
+    myNumBales = cowhay;
+  }
+
+  public int getWeight() {return myWeight;}
+
+  public double value(double perPound) {return myPounds * perPound;}
+
+  public int getCorn() {return myCorn;}
+
+  public int getHay() {return myNumBales;}
+
+  public double getCost(int cornCost, int hayCost) {return (cornCost * myCorn) + (hayCost + myNumBales);}
+}
