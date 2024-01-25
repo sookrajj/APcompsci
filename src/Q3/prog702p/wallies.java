547546e3c3a90e16aa6b7f38da9a9d56e9f6d088
sookrajj <144150804+sookrajj@users.noreply.github.com>
2024-01-25 12:51:40 -0600

    hi

--- src/Q3/prog702p/wallies.java
+++ src/Q3/prog702p/wallies.java
@@ -2,22 +2,12 @@ package Q3.prog702p;
 
 public class wallies extends Animals{
     private int mysteps;
-    private static int allsteps = 0;
-    private double average;
-    private static int cnt = 0;
+    
 
     public wallies(String name, String word, int steps) {
         super(name, word);
-        mysteps = steps;
-        average = 0;
-        allsteps += steps;
-        cnt++;
-    }
-
-    public void calc() {
-        average = (double) allsteps/cnt;
     }
 
     public int getSteps() {return mysteps;}
-    public double getAve() {return average;}
+    
 }
