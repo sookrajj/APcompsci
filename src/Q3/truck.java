73cd3f1d7ce1461621e41f315d4dbaf076a87c43
sookrajj <144150804+sookrajj@users.noreply.github.com>
2024-01-29 14:51:13 -0600

    hi

---
+++ src/Q3/truck.java
@@ -0,0 +1,11 @@
+package Q3;
+
+public class truck extends vehicles {
+  private double mileage;
+
+  public truck(String name, int tires, double val, double miles) {
+    super(name, tires, val);
+  }
+}
+
+  
