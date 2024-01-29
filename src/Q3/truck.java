6e7ecbe8b57de24907a7b76d9fdbaa2b817743cd
sookrajj <144150804+sookrajj@users.noreply.github.com>
2024-01-29 14:55:17 -0600

    hi

--- src/Q3/truck.java
+++ src/Q3/truck.java
@@ -5,7 +5,12 @@ public class truck extends vehicles {
 
   public truck(String name, int tires, double val, double miles) {
     super(name, tires, val);
+    mileage = miles;
   }
+
+
+  public double getmileage() {return mileage;}
+  
 }
 
   
