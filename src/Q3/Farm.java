52a5ce380be7892d97d33def0b780e774d0f7884
sookrajj <144150804+sookrajj@users.noreply.github.com>
2024-01-31 14:37:01 -0600

    hi

---
+++ src/Q3/Farm.java
@@ -0,0 +1,110 @@
+public class Farm implements Ifarm2 {
+  private ArrayList<Cow2> cow;
+  private ArrayList<Horse2> gay;
+  private int myNumHayBales;
+	private int myNumCorn;
+	private double myHayCost;
+	private double myCornCost;
+
+
+  public Farm(ArrayList<Cow2> cow, ArrayList<Horse2> gay, int numHaybales, int numCorn, double HayCost, double CornCost) {
+    this.cow = cow;
+    this.gay = gay;
+    myNumHaybales = numHaybales;
+    myNumCorn = numCorn;
+    myHayCost = HayCost;
+    myCornCost = CornCost;
+  }
+
+  public boolean feedCows() {
+    int hay = 0;
+    int corn = 0;
+    for (Cow2 co : cow) {
+      hay += co.getHay();
+      corn += co.getCorn();
+    }
+    if (hay <= myNumHaybales && corn <= myNumCorn) return true;
+    return false;
+  }
+
+  public boolean feedHorses() {
+    int hay = 0;
+    int corn = 0;
+    for (Horse2 ho : gay) {
+      hay += ho.getHay();
+      corn += ho.getCorn();
+    }
+    if (hay <= myNumHaybales && corn <= myNumCorn) return true;
+    return false;
+  }
+
+  public boolean feedCows() {
+    int hay = 0;
+    int corn = 0;
+    for (Cow2 co : cow) {
+      hay += co.getHay();
+      corn += co.getCorn();
+    }
+    for (Horse2 ho : gay) {
+      hay += ho.getHay();
+      corn += ho.getCorn();
+    }
+    if (hay <= myNumHaybales && corn <= myNumCorn) return true;
+    return false;
+  }
+
+  public double cowIncome(double perPound) {
+    double milk = 0;
+    for (Cow2 co : cow) {
+      milk += co.getMilk() * perPound;
+    }
+    return milk;
+  }
+
+  public double horseIncome() {
+    double ride = 0;
+    for (Horse2 ho : gay) {
+      ride += ho.getRides() * ho.getRideCost();
+    }
+    return ride;
+  }
+
+  public double farmIncome(double perPound) {
+    double milk = 0;
+    for (Cow2 co : cow) {
+      milk += co.getMilk() * perPound;
+    }
+    double ride = 0;
+    for (Horse2 ho : gay) {
+      ride += ho.getRides() * ho.getRideCost();
+    }
+    return milk + ride;
+  }
+
+  public int farmWeight() {
+    int cowWeight = 0;
+    for (Cow2 co : cow) {
+      cowWeight += co.getWeight();
+    }
+    int horseWeight = 0;
+    for (Horse2 ho : gay) {
+      horseWeight += ho.getWeight();
+    }
+    return milk + ride;
+  }
+
+  public double farmCost() {
+    double milk = 0;
+    for (Cow2 co : cow) {
+      milk += co.getCost();
+    }
+    double ride = 0;
+    for (Horse2 ho : gay) {
+      ride += ho.getCost();
+    }
+    return milk + ride;
+  }
+
+  public ArrayList<Cow2> cows() {return cow;}
+  public ArrayList<Horse2> gay() {return gay;}
+}
