7b71508f39743f385ccbf11f2805bc0ec039d002
sookrajj <144150804+sookrajj@users.noreply.github.com>
2024-01-31 09:51:14 -0600

    hi

---
+++ src/Q3/Computer.java
@@ -0,0 +1,15 @@
+public class Computer implements comps {
+  private String name;
+  private int number;
+  private double value;
+
+  public Computer(String name, int number, double value) {
+    this.name = name;
+    this.number = number;
+    this.value = value;
+  }
+
+  public String getName() {return name;}
+  public int getNumber() {return number;}
+  public double getValue() {return value;}
+}
