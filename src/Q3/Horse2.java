94cccd2e60a956cdacf5042baf9a3f93a0b60147
sookrajj <144150804+sookrajj@users.noreply.github.com>
2024-02-01 10:34:00 -0600

    hi

--- src/Q3/Horse2.java
+++ src/Q3/Horse2.java
@@ -12,18 +12,23 @@ public class Horse2 {
     mynumRides = numRides;
     mycostRide = rideCost;
   }
+  
+public void changeWeight(int weight) {myweight = weight;}
+public void changeCorn(int corn) {mycorn = corn;}
+public void changeWeight(int haybales) {myHaybales = haybales;}
+public void changeWeight(int numRides) {mynumRides = numRides;}
+public void changeWeight(double rideCost) {myrideCost = rideCost;}
+  public int getWeight() {return myweight;}
 
-  Public int getWeight() {return myweight;}
+//returns the value of the milk produced
+public double value() {return mynumRides*mycostRide;}
 
-	//returns the value of the milk produced
-	Public double value() {return mynumRides*mycostRide;}
+//returns the amount of corn eaten by this cow
+public int getCorn() {return myCorn;}
 
-	//returns the amount of corn eaten by this cow
-	Public int getCorn() {return myCorn;}
+//returns the amount of hay eaten by this cow
+public int getHay() {return myHaybales;}
 
-	//returns the amount of hay eaten by this cow
-	Public int getHay() {return myHaybales;}
-
-	//calculates the amount of money it will take to feed the cows for the day
-	Public double getCost(double corncost,double haycost) {return (corncost*mycorn) + (haycost * myHaybales);}
+//calculates the amount of money it will take to feed the cows for the day
+public double getCost(double corncost,double haycost) {return (corncost*mycorn) + (haycost * myHaybales);}
 }
