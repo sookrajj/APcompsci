1cb0e29cbb0804fc7b01dfc4f30a98b00edab071
sookrajj <144150804+sookrajj@users.noreply.github.com>
2024-02-01 07:46:57 -0600

    hi

---
+++ src/Q3/Cow2.java
@@ -0,0 +1,27 @@
+public class Cow2 {
+  Private int myweight;
+	Private int mymilk;
+	Private int mycorn;
+	Private int myHaybales;
+
+  public Cow2(int weight, int milk, int corn, int HayEaten) {
+    myweight = weight;
+    mymilk = milk;
+    mycorn = corn;
+    myHaybales = HayEaten;
+  }
+
+  Public int getWeight() {return myweight;}
+
+	//returns the value of the milk produced
+	Public double value(double perpound) {return perPound * milk;}
+
+	//returns the amount of corn eaten by this cow
+	Public int getCorn() {return myCorn;}
+
+	//returns the amount of hay eaten by this cow
+	Public int getHay() {return myHaybales;}
+
+	//calculates the amount of money it will take to feed the cows for the day
+	Public double getCost(double corncost,double haycost) {return (corncost*mycorn) + (haycost * mynumbales);}
+}
