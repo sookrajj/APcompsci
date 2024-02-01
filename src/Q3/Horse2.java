a1dd49fd9c93e697bf77807f6cbb16060072afa5
sookrajj <144150804+sookrajj@users.noreply.github.com>
2024-02-01 07:51:15 -0600

    hi

---
+++ src/Q3/Horse2.java
@@ -0,0 +1,29 @@
+public class Horse2 {
+  private int myweight;
+	private int mynumRides;
+	private int mycorn;
+	private int myHaybales;
+  private double mycostRide;
+
+  public Cow2(int weight, int corn, int HayEaten, int numRides, double rideCost) {
+    myweight = weight;
+    mycorn = corn;
+    myHaybales = HayEaten;
+    mynumRides = numRides;
+    mycostRide = rideCost;
+  }
+
+  Public int getWeight() {return myweight;}
+
+	//returns the value of the milk produced
+	Public double value(double perpound) {return mynumRides*mycostRide;}
+
+	//returns the amount of corn eaten by this cow
+	Public int getCorn() {return myCorn;}
+
+	//returns the amount of hay eaten by this cow
+	Public int getHay() {return myHaybales;}
+
+	//calculates the amount of money it will take to feed the cows for the day
+	Public double getCost(double corncost,double haycost) {return (corncost*mycorn) + (haycost * myHaybales);}
+}
