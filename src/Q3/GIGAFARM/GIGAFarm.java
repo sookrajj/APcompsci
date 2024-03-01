a1623b075c68a32518b1cc1ff68241bf732bfe7b
sookrajj <144150804+sookrajj@users.noreply.github.com>
2024-02-29 20:53:42 -0600

    hi

--- src/Q3/GIGAFARM/GIGAFarm.java
+++ src/Q3/GIGAFARM/GIGAFarm.java
@@ -4,7 +4,8 @@ import Q2.ArrayListcl.ArrayListab;
 
 import java.util.ArrayList;
 import java.util.Map;
-
+import java.util.ListedList;
+import java.util.Set;
 public class GIGAFarm {
 
     private ArrayList<GIGAAnimals> anis;
@@ -14,12 +15,17 @@ public class GIGAFarm {
     private int curind;
     private double[] coast = new double[5];
     private Map<Integer, GIGACow> cowies;
+    private CircleListedList<GIGAHorse> horse;
+    private Set<GIGATurkey> turk;
+    private GIGAPigs[] Pigs;
 
-    public GIGAFarm(ArrayList<GIGAAnimals> animals, Map<Integer, GIGACow> cows, int[][] rides, int[] nums, double[] cost) {
+    public GIGAFarm(GIGAPigs[] pig, Map<Integer, GIGACow> cows, CircleListedList<GIGAHorse> horse, Set<GIGATurkey> turks, int[][] rides, int[] nums, double[] cost) {
          anis = animals;
          numes = nums;
          rids = rides;
          cowies = cows;
+         turk = turks;
+         Pigs = pig;
          for (int lcv = 0; lcv < animals.size(); lcv++) {
              if (animals.get(lcv) instanceof GIGAHorse && firhor == 0) {
                  firhor = lcv;
@@ -27,11 +33,14 @@ public class GIGAFarm {
          }
          curind = firhor;
          coast = cost;
+        this.horse = horse;
     }
 
     public int getCurind() {return curind;}
     public int getfirhor() {return firhor;}
     public ArrayList<GIGAAnimals> getAnis() {return anis;}
+    public Set<GIGATurkey> getTurk() {return turk;}
+    public GIGAPig[] getPigs() {return pigs;}
 
     public double getBc() {
         return coast[2];
@@ -54,6 +63,9 @@ public class GIGAFarm {
     }
     public Map<Integer, GIGACow> getCowies() {
         return cowies;
+    }
+    public CircleListedList<GIGAHorse> getHorse() {
+        return horse;
     }
     public int getCorn() {return numes[0];}
     public int getHay() {return numes[1];}
