d70c5200d9135dd124c2d34d9febb6c8d40fb819
sookrajj <144150804+sookrajj@users.noreply.github.com>
2024-04-04 15:22:46 -0500

    hi

--- src/Q4/VirtualPetLab/PetManager.java
+++ src/Q4/VirtualPetLab/PetManager.java
@@ -5,6 +5,17 @@ public class PetManager {
     private ArrayList<Pet> pets = new ArrayList<>();
 
     // TODO: Implement methods as needed to interact with the pets list
-    public void addPet(Pet pet) {}
+    public void addPet(Pet pet) {
+        pets.add(pet);
+    }
+    public void removePet(String name) {
+        for (int lcv = 0; lcv < pets; lcv++) {
+            if (pets.get(lcv).getName().equals(name)) {
+                pets.remove(lcv);
+            }
+        }
+    }
+
+    public ArrayList<Pet> getPets() { return pets;}
 
 }
