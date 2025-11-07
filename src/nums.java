73bf64a99ab28534b4c458f68eb473584c0af7b0
sookrajj <144150804+sookrajj@users.noreply.github.com>
2025-11-07 11:14:20 -0600

    hi

--- src/nums.java
+++ src/nums.java
@@ -22,38 +22,45 @@ public class nums {
                     int n2 = nums[i+1];
                     increasing += n1 < n2 ? 1 : 0;
                     decreasing += n1 > n2 ? 1 : 0;
-                    tols += n1 == n2 ? 1 : 0;
-                    if (count == 1) System.out.println(tols + " " + increasing + " " + decreasing);
+                    // tols += n1 == n2 ? 1 : 0;
+                    if (count == 1) System.out.println(increasing + " " + decreasing);
                 }
                 if (increasing > decreasing) {
-                    tols += decreasing > 0 ? decreasing : 0;
+                    // tols += decreasing > 0 ? decreasing : 0;
+                    var boom = false;
                     for (int i = 0; i < nums.length-1; i++) {
                         int n1 = nums[i];
                         int n2 = nums[i+1];
-                        if (n1 >= n2 && i+2 !=  nums.length) {
-                            n2 = nums[i+2];
-                            i++;
-                        } else if (n1 >= n2) {
-                            break;
-                        }
-                        if (n1 - n2 < -3) tols++;
+                        // if ((n1 >= n2 || n1 - n2 < -3) && i+2 !=  nums.length) {
+                        //     n2 = nums[i+2];
+                        //     i++;
+                        if (boom && (n1 - n2 < -3 || n1 - n2 >= 0)) {tols = 10000; break;}
+                        boom = boom || (n1 - n2 < -3 || n1 - n2 >= 0);
+                        // } else if (n1 >= n2) {
+                        //     break;
+                        // }
+                        // if (n1 - n2 < -3 || n1 - n2 >= 0) tols++;
                     }
                 } else if (increasing < decreasing) {
-                    tols += increasing > 0 ? increasing : 0;
+                    // tols += increasing > 0 ? increasing : 0;
+                    var boom = false;
                     for (int i = 0; i < nums.length-1; i++) {
                         int n1 = nums[i];
                         int n2 = nums[i+1];
-                        if (n1 <= n2 && i+2 !=  nums.length) {
-                            n2 = nums[i+2];
-                            i++;
-                        } else if (n1 <= n2) {
-                            break;
-                        }
-                        if (n1 - n2 > 3) tols++;
+                        // if (n1 <= n2 && i+2 !=  nums.length) {
+                        //     n2 = nums[i+2];
+                            // i++;
+                        if (boom && (n1 - n2 > 3 || n1 - n2 <= 0)) {tols = 10000; break;}
+                        boom = boom || (n1 - n2 > 3 || n1 - n2 <= 0);
+                        // } else if (n1 <= n2) {
+                        //     break;
+                        // }
+                        // if (n1 - n2 > 3 || n1 - n2 <= 0) tols++;
                     }
                 } else {tols += 3;}
-                if (count == 1) {System.out.println();System.out.print(tols + " ");}
+                // if (count == 1) {System.out.println();System.out.print(tols + " ");}
                 count += tols < 2 ? 1 : 0;
+                // count += boom ? 0 : 1;
                 
             }
             System.out.println(count);
