d0efa0319123cf2a5a73485b28a251d66b87c89b
sookrajj <144150804+sookrajj@users.noreply.github.com>
2025-11-07 19:13:02 +0000

    hi

--- src/nums.java
+++ src/nums.java
@@ -10,32 +10,41 @@ public class nums {
             while (input.hasNext()) {
                 String line = input.nextLine();
                 String[] newline = line.split(" ");
-                int[] nums = new int[newline.length];
+                ArrayList<Integer> nums = new ArrayList<>();
                 for (int i = 0; i < newline.length; i++) {
-                    nums[i] = Integer.parseInt(newline[i]);
+                    nums.add(Integer.parseInt(newline[i]));
                 }
                 int increasing = 0;
                 int decreasing = 0;
                 int tols = 0;
-                for (int i = 0; i < nums.length-1; i++) {
-                    int n1 = nums[i];
-                    int n2 = nums[i+1];
+                for (int i = 0; i < nums.size()-1; i++) {
+                    int n1 = nums.get(i);
+                    int n2 = nums.get(i+1);
                     increasing += n1 < n2 ? 1 : 0;
                     decreasing += n1 > n2 ? 1 : 0;
                     // tols += n1 == n2 ? 1 : 0;
-                    if (count == 1) System.out.println(increasing + " " + decreasing);
+                    // System.out.println(increasing + " " + decreasing);
                 }
+                var boom = false;
                 if (increasing > decreasing) {
                     // tols += decreasing > 0 ? decreasing : 0;
-                    var boom = false;
-                    for (int i = 0; i < nums.length-1; i++) {
-                        int n1 = nums[i];
-                        int n2 = nums[i+1];
+        
+                    for (int i = 0; i < nums.size()-1; i++) {
+                        int n1 = nums.get(i);
+                        int n2 = nums.get(i+1);
                         // if ((n1 >= n2 || n1 - n2 < -3) && i+2 !=  nums.length) {
                         //     n2 = nums[i+2];
                         //     i++;
-                        if (boom && (n1 - n2 < -3 || n1 - n2 >= 0)) {tols = 10000; break;}
+                        if (boom && (n1 - n2 < -3 || n1 - n2 >= 0)) {
+                            tols = 10000; 
+                            // System.out.println("hi");
+                            break;
+                        }
                         boom = boom || (n1 - n2 < -3 || n1 - n2 >= 0);
+                        if (boom && (n1 - n2 < -3 || n1 - n2 >= 0)){
+                            nums.remove(i+1);
+                            i--;
+                        } 
                         // } else if (n1 >= n2) {
                         //     break;
                         // }
@@ -43,22 +52,36 @@ public class nums {
                     }
                 } else if (increasing < decreasing) {
                     // tols += increasing > 0 ? increasing : 0;
-                    var boom = false;
-                    for (int i = 0; i < nums.length-1; i++) {
-                        int n1 = nums[i];
-                        int n2 = nums[i+1];
+                    // var boom = false;
+                    for (int i = 0; i < nums.size()-1; i++) {
+                        int n1 = nums.get(i);
+                        int n2 = nums.get(i+1);
                         // if (n1 <= n2 && i+2 !=  nums.length) {
                         //     n2 = nums[i+2];
                             // i++;
-                        if (boom && (n1 - n2 > 3 || n1 - n2 <= 0)) {tols = 10000; break;}
+                        if (boom && (n1 - n2 > 3 || n1 - n2 <= 0)) {
+                            // System.out.println("hi");
+                            tols = 10000; 
+                            break;
+                        }
                         boom = boom || (n1 - n2 > 3 || n1 - n2 <= 0);
+                        if (boom && (n1 - n2 < -3 || n1 - n2 >= 0)) {
+                            nums.remove(i+1);
+                            i--;
+                        }
                         // } else if (n1 <= n2) {
                         //     break;
                         // }
                         // if (n1 - n2 > 3 || n1 - n2 <= 0) tols++;
                     }
                 } else {tols += 3;}
-                // if (count == 1) {System.out.println();System.out.print(tols + " ");}
+                // System.out.println(boom);
+                // if (count == 0) {
+                //     for (int i = 0; i < nums.size(); i++) {
+                //         System.out.print(nums.get(i));
+                //     }
+                //     System.out.println();
+                // }
                 count += tols < 2 ? 1 : 0;
                 // count += boom ? 0 : 1;
                 
